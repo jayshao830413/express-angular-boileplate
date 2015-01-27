0fa21ee3c41a6398110aa0b4149206feaab9e758
jayshao830413 <shaoshijie830413@foxmail.com>
2015-01-27 04:14:57 -0500

    change the position of server hosted

--- server/app.js
+++ server/app.js
@@ -16,38 +16,37 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
+/*
+* Development  Settings
+*/
 if (app.get('env') === 'development') {
+    app.use(express.static(path.join(__dirname, '../client')));
+
+    app.use(express.static(path.join(__dirname, '../client/.tmp')));
+    app.use(express.static(path.join(__dirname, '../client/app')));
+
+    //Error Handling
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
-        res.render('error', {
+        res.render('err', {
             message: err.message,
             error: err
         });
     });
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
+if (app.get('env') === 'production') {
+    app.use(express.static(path.join(__dirname, '/dist')));
+    // production error handler
+    // no stacktraces leaked to user
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: {}
+        });
     });
-});
-
+}
 
 module.exports = app;
